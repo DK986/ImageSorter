e8d8e23d8b098879d8c9ce19a681ad4f17fd3bae
Denis <Denis@DESKTOP-L4GF7CP>
2020-01-20 00:14:24 +0100

    Init

---
+++ ImageSorter/ImageViewControl.xaml.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ImageSorter
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class ImageViewControl : Window
+    {
+        private ImageViewViewModel _model;
+
+        public ImageViewControl()
+        {
+            InitializeComponent();
+            DataContext = _model = new ImageViewViewModel();
+        }
+
+        private void Load_Click(object sender, RoutedEventArgs e)
+        {
+            _model.setup();
+        }
+
+        private void Settings_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Window_KeyDown(object sender, KeyEventArgs e)
+        {
+            _model.changeImage(e);
+        }
+    }
+}
---
+++ ImageSorter/ImageViewViewModel.cs
@@ -0,0 +1,140 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Windows.Input;
+using System.Windows.Media.Imaging;
+
+namespace ImageSorter
+{
+    public class ImageViewViewModel : INotifyPropertyChanged
+    {
+        private List<string> _files;
+
+        public BitmapImage Image { get; set; }
+        private int index = 0;
+
+        public ImageViewViewModel()
+        {
+
+        }
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public void setup()
+        {
+            FolderBrowserDialog dialog = new FolderBrowserDialog();
+            _files = new List<string>();
+            if (dialog.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            {
+                var ext = new List<string> { ".jpg", ".jpeg", ".tif", ".gif", ".png", ".jfif" };
+                var dir = dialog.SelectedPath;
+                _files = Directory.GetFiles(dir, "*.*", SearchOption.AllDirectories).OrderBy(i => System.IO.Path.GetExtension(i)).Where(s => ext.Contains(System.IO.Path.GetExtension(s))).ToList();
+            }
+            if (_files.Count > 0)
+                setImage(_files[index]);
+            else
+            {
+                var res = MessageBox.Show("Es wurden keine Bilder gefunden", "Fehler", MessageBoxButtons.OK);
+                if (res == DialogResult.OK)
+                    setup();
+            }
+        }
+
+        private void setImage(string file, bool rotate = false)
+        {
+            var stream = new MemoryStream(File.ReadAllBytes(file));
+            stream.Position = 0;
+
+
+            BitmapImage bmpImage = new BitmapImage();
+
+            bmpImage.BeginInit();
+            bmpImage.StreamSource = stream;
+            bmpImage.CacheOption = BitmapCacheOption.OnLoad;
+            if (rotate)
+                bmpImage.Rotation = Rotation.Rotate0;
+            bmpImage.EndInit();
+            bmpImage.Freeze();
+            Image = bmpImage;
+        }
+
+        private void moveImage(string file, string path)
+        {
+            File.Move(file, System.IO.Path.Combine(path, System.IO.Path.GetFileName(file)));
+        }
+
+        public void changeImage(System.Windows.Input.KeyEventArgs e)
+        {
+            if (e.Key == Key.Left)
+            {
+                if (index < _files.Count)
+                    setImage(_files[index], true);
+            }
+            else
+            {
+                string path = "";
+                bool move = false;
+                switch (e.Key)
+                {
+                    case Key.NumPad0:
+                        {
+                            path = @"D:\Archiv\Bilder\Trash";
+                            move = true;
+                            break;
+                        }
+                    case Key.NumPad1:
+                        {
+                            path = @"D:\Archiv\Bilder\Bratanzlatan";
+                            move = true;
+                            break;
+                        }
+                    case Key.NumPad2:
+                        {
+                            path = @"D:\Archiv\Bilder\Bruhs - Parkour - Squad";
+                            move = true;
+                            break;
+                        }
+                    case Key.NumPad3:
+                        {
+                            path = @"D:\Archiv\Bilder\Fam";
+                            move = true;
+                            break;
+                        }
+                    case Key.NumPad4:
+                        {
+                            path = @"D:\Archiv\Bilder\hotstuff";
+                            move = true;
+                            break;
+                        }
+                    case Key.NumPad5:
+                        {
+                            path = @"C:\Users\Denis\Desktop\Fam";
+                            move = true;
+                            break;
+                        }
+                    case Key.NumPad6:
+                        {
+                            path = @"D:\Archiv\Bilder\Unsortiert";
+                            move = true;
+                            break;
+                        }
+                }
+                if (move)
+                {
+                    moveImage(_files[index], path);
+                    index++;
+                    if (index < _files.Count)
+                        setImage(_files[index]);
+                    else
+                    {
+                        MessageBox.Show("Keine Bilder mehr");
+                    }
+                }
+            }
+        }
+    }
+}
